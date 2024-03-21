89363b54e3b97194dd71909ab394a97485b21947
TrangPC <nguyenthutrang542002@gmail.com>
2024-03-21 18:10:39 +0700

    commit

--- API_postgreDB/__init__.py
+++ API_postgreDB/__init__.py
@@ -1,27 +1,53 @@
-from flask import Flask, jsonify, request, SQLALchemy
-from sqlalchemy import Column, Integer, String, Boolean
+from flask import Flask, request, jsonify
 import psycopg2
 app = Flask(__name__)
-app.secret_key = 'gjla14234&&o2824jfsj##$gjlajlaaqpri'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@hostname:port/database_name'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLALchemy(app)
-
-app.route('/data/profile', methods = ['POST'])
+hostname = 'localhost'
+database = 'Week2'
+username = 'postgres'
+password = '12345678'
+port = '5432'
 def connect():
-    pass
-class Profile(db.Model):
-    id = Column(Integer, nullable=False, autoincrement=True)
-    name = Column(String(50))
-    address = Column(String(100))
-    job = Column(String(100))
-    phone_number = Column(String(100))
+    try:
+        conn = psycopg2.connect(
+            host = hostname,
+            dbname = database,
+            user = username,
+            password = password,
+            port = port
+        )
+        cur = conn.cursor()
+        create_table ='''   CREATE TABLE IF NOT EXISTS profile(
+                                id int PRIMARY KEY,
+                                name varchar(50) NOT NULL,
+                                address varchar(100),
+                                job varchar(100),
+                                phone_number varchar(15))'''
+        cur.execute(create_table)
+        conn.commit()
+        return conn
+    except Exception as error:
+        print(str(error))
+        cur.close()
+        conn.close()
+@app.route('/data/profile', methods = ['POST'])
+def get_profile():
+    try:
+        cur = connect().cursor()
+
+        name = request.json.get('name')
+        cur.execute("SELECT * FROM profile WHERE name = %s;", (name,))
+        profile_data = cur.fetchone()
+        return jsonify(
+            profile_data
+            , 200)
+
+    except Exception as error:
+        return jsonify({
+            'status': 'faild'
+            }, 400)
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
 
